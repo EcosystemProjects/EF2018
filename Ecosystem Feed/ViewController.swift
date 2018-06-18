555b66c952320b163e6273dfb9d0b8864655b5ec
Ahmet Peşman <ahmetbugrapesman@ahmet-macbook-pro.local>
2018-06-18 15:06:06 +0300

    Initial Commit

---
+++ Ecosystem Feed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ecosystem Feed
+//
+//  Created by Ahmet Buğra Peşman on 18.06.2018.
+//  Copyright © 2018 Ahmet Buğra Peşman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
