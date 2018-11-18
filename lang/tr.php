6ce078b2caa224b9b591e696ee23d5d5bba64061
beratkara <beratkara@windowslive.com>
2018-11-19 01:40:06 +0300

    Add files via upload

---
+++ lang/tr.php
@@ -0,0 +1,15 @@
+<?php
+	define("Feed","Besleme");
+	define("Ecosystems","Ekosistemler");
+	define("About","Hakkında");
+	define("Turkish","Türkçe");
+	define("English","İngilizce");
+	define("ConnectLinkedin","Linkedin İle Giriş Yap");
+	
+	/* homepage string */
+	define("HomepageConnectEcosystem","Ekosisteminize bağlanın!");
+	define("HomepageConnectEcosystemComment","Ekosistem Beslemeleri, iş ve sosyal ekosistemler hakkında yeni mesajlar almanın en iyi yoludur.");
+	define("Explore","Keşfet");
+	define("DownloadApps","Uygulamayı İndir");
+	
+?>
