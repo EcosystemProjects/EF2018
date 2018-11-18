beaa3af7f79975ea77576d4b9bcf075ac215d565
beratkara <beratkara@windowslive.com>
2018-11-19 01:40:48 +0300

    Add files via upload

---
+++ Backend/lang/en.php
@@ -0,0 +1,14 @@
+<?php
+	define("Feed","Feed");
+	define("Ecosystems","Ecosystems");
+	define("About","About");
+	define("Turkish","Turkish");
+	define("English","English");
+	define("ConnectLinkedin","Connect via Linkedin");
+	
+	/* homepage string */
+	define("HomepageConnectEcosystem","Connect to your ecosystem!");
+	define("HomepageConnectEcosystemComment","Ecosystem Feed is the best way to get fresh posts about business and social ecoystems.");
+	define("Explore","Explore");
+	define("DownloadApps","Download Apps");
+?>
