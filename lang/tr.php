d8e562fc0e7b2b0fc15e1fb8a99654b0db981f94
beratkara <beratkara@windowslive.com>
2018-11-20 06:37:05 +0300

    Add files via upload

--- lang/tr.php
+++ lang/tr.php
@@ -1,15 +1,63 @@
 <?php
 	define("Feed","Besleme");
 	define("Ecosystems","Ekosistemler");
+	define("Ecosystem","Ekosistem");
 	define("About","Hakkında");
 	define("Turkish","Türkçe");
 	define("English","İngilizce");
 	define("ConnectLinkedin","Linkedin İle Giriş Yap");
 	
-	/* homepage string */
+	/* homepage.php string */
 	define("HomepageConnectEcosystem","Ekosisteminize bağlanın!");
 	define("HomepageConnectEcosystemComment","Ekosistem Beslemeleri, iş ve sosyal ekosistemler hakkında yeni mesajlar almanın en iyi yoludur.");
 	define("Explore","Keşfet");
 	define("DownloadApps","Uygulamayı İndir");
 	
+	/* menu.php string */
+	define("HOMEPAGE","Ana Sayfa");
+	define("ADMINPAGE","Admin Paneli");
+	define("Createpost","Gönderi Oluştur");
+	define("Settings","Ayarlar");
+	define("Logout","Çıkış Yap");
+	
+	/* follower.php string */
+	define("existsfollowuser","Zaten Takiptesin !");
+	define("followsuccess","Başarılı bir şekilde takip edildi !");
+	define("unfollowsuccess","Başarılı bir şekilde takipten çıkıldı !");
+	define("Followers","Takipçi");
+	define("Post","Gönderi");
+	define("Follow","Takip Et");
+	define("Unfollow","Takipten Çık");
+	
+	/* region.php string */
+	define("Region","Bölge");
+	define("SeeEcosystemWithRegion","Ekosistemleri görmek için bir bölge seçin");
+	define("NotFoundRegion","Henüz Bölge Yok !");
+	
+	/* ecosystem.php string */
+	define("SeeCategoryWithEcosystem","Kategorileri görmek için bir ekosistem seçin");
+	define("NotFoundEcosystem","Henüz Ekosistem Yok !");
+	
+	/* category.php string */
+	define("Category","Kategori");
+	define("Categorys","Kategoriler");
+	define("NotFoundCategory","Henüz Kategori Yok !");
+	
+	/* categoryposts.php string */
+	define("NotFoundPost","Henüz Paylaşım Yok !");
+	define("More","Devamı");
+	
+	/* home.php string */
+	define("NotFoundPostsWithCategory","Henüz Takip Ettiğiniz Kategorilerde Paylaşım Yapılmamış ! ");
+	
+	/* settings.php string */
+	define("Contact","İletişim");
+	define("Languages","Desteklenen Diller");
+	define("SendMeMobileNotifications","Takip ettiğim kategorilerde öne çıkan gönderi oluşturulduğunda bana cep bildirimleri gönder");
+	define("SendMeEmailNotifications","Takip ettiğim kategorilerde öne çıkan gönderi  oluşturulduğunda bana E-mail bildirimleri gönder");
+	
+	define("FOLLOWUS","BİZİ TAKİP ET");
+	define("WRITEUS","BİZE YAZIN");
+	define("VISITWEBPAGE","İNTERNET SİTEMİZİ ZİYARET EDİN");
+	define("ABOUTMESSAGE","Bu Proje Mentornity Inc.'in Büyük Stajyer Ekipleri Tarafından Geliştirilen Bir Dahili Projedir . Onlarla Tanışın ve Linkedin Profillerini Ziyaret Edin .");
 ?>
