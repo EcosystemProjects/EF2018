580da22b1ac7c6b1402b5d4bf64ae9275a27db49
beratkara <beratkara@windowslive.com>
2018-11-23 05:45:18 +0300

    Add files via upload

--- Backend/page/admin/posts.php
+++ Backend/page/admin/posts.php
@@ -21,30 +21,35 @@
 				$postsid = @$_GET['posts'];
 				$mail = @$_GET['mail'];
 				$app = @$_GET['app'];
+				$catidget = @$_GET['catid'];
 				
 				if($mail && ($mail != "on" && $mail != "off"))
 					return;
 				if($app && ($app != "on" && $app != "off"))
 					return;
 				
-				$query = $DBFunctions->selectAll("SELECT notification FROM posts WHERE id = $postsid");
+				$query = $DBFunctions->selectAll("SELECT id,notification FROM posts WHERE id = $postsid");
 				if(count($query))
 				{
 					$data = $DBFunctions->PDO_fetch_array($query, 0);
+					$postid = $data['id'];
 					$notification = json_decode($data['notification'],JSON_UNESCAPED_UNICODE);
 					if($mail)
-						if($mail == "on")
+						if($mail == "on"){
 							$notification['mail'] = 1;
-						elseif($mail == "off")
+							$db->query("INSERT INTO `process` (`id`, `type`, `postid`, `catid`) VALUES (NULL, '1', '$postid', '$catidget')");//type 1 - mail sent 
+						}elseif($mail == "off")
 							$notification['mail'] = 0;
 					if($app)
-						if($app == "on")
+						if($app == "on"){
 							$notification['app'] = 1;
-						elseif($app == "off")
+							$db->query("INSERT INTO `process` (`id`, `type`, `postid`, `catid`) VALUES (NULL, '2', '$postid', '$catidget')");//type 2 - application sent
+						}elseif($app == "off")
 							$notification['app'] = 0;
 					
 					$notification = json_encode($notification,JSON_UNESCAPED_UNICODE);
 					$db->query("UPDATE posts SET notification = '$notification' WHERE id = '$postsid'");
+						
 				}
 				
 			}
@@ -70,7 +75,7 @@
 				if (ob_get_level() == 0)
 					ob_start();
 				
-				$query = $DBFunctions->selectAll("SELECT r.name as rname,e.name as ename,c.name as cname,p.id as id,p.information as information,p.sender as sender,p.categoryid as categoryid,p.notification as notification,p.status as status FROM posts as p,category as r,category as e,category as c WHERE p.categoryid = c.id and e.id = c.groupid and r.id = e.groupid");
+				$query = $DBFunctions->selectAll("SELECT r.name as rname,e.name as ename,c.name as cname,c.id as catid,p.id as id,p.information as information,p.sender as sender,p.categoryid as categoryid,p.notification as notification,p.status as status,p.seourl as seourl FROM posts as p,category as r,category as e,category as c WHERE p.categoryid = c.id and e.id = c.groupid and r.id = e.groupid");
 				
 				if (count($query) == 0) {
 					echo '<tr><td>Henüz Post Yok ! </td></tr>';
@@ -87,6 +92,8 @@
 						$postsnotification = json_decode($data['notification'],JSON_UNESCAPED_UNICODE);
 						$postsmail = $postsnotification['mail'];
 						$postsapp = $postsnotification['app'];
+						$catid = $data['catid'];
+						$seourl = $data['seourl'];
 						
 						$userid = $data['sender'];
 						$userquery = $DBFunctions->selectAll("SELECT information,email FROM users WHERE id = $userid");
@@ -97,7 +104,7 @@
 						<tr>
 							<form method=\"post\">
 							<input type=\"hidden\" name=\"posts\" value=\"$postsid\" />
-							<td>".$postsdatainf['title']."</td>
+							<td><a href=\"dashboard/posts/".$catid."/".$seourl.".html\" target=\"blank\">".$postsdatainf['title']."</a></td>
 							<td>".$userdatainf['name']." ".$userdatainf['surname']."</td>
 							<td>".$useremail."</td>
 							<td>".$data['rname']."</td>
@@ -111,10 +118,10 @@
 							  </select>
 							</td>
 							<td>
-								".($postsmail ? "<a class=\"GreyButton\" href=\"admin/posts.html?posts=$postsid&mail=off\">SEND OFF</a>" : "<a class=\"GreyButton\" href=\"admin/posts.html?posts=$postsid&mail=on\">SEND ON</a>")."
+								".($postsmail ? "<a class=\"GreyButton\" href=\"admin/posts.html?posts=$postsid&mail=off&catid=$catid\">SEND OFF</a>" : "<a class=\"GreyButton\" href=\"admin/posts.html?posts=$postsid&mail=on&catid=$catid\">SEND ON</a>")."
 							</td>
 							<td>
-								".($postsapp ? "<a class=\"GreyButton\" href=\"admin/posts.html?posts=$postsid&app=off\">SEND OFF</a>" : "<a class=\"GreyButton\" href=\"admin/posts.html?posts=$postsid&app=on\">SEND ON</a>")."
+								".($postsapp ? "<a class=\"GreyButton\" href=\"admin/posts.html?posts=$postsid&app=off&catid=$catid\">SEND OFF</a>" : "<a class=\"GreyButton\" href=\"admin/posts.html?posts=$postsid&app=on&catid=$catid\">SEND ON</a>")."
 							</td>
 							<noscript><input type=\"submit\" name=\"editposts\" value=\"Submit\"></noscript>
 							</form>
